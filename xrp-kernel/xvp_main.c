26bcbaa54d453d2f2ea5a1c8f8da62bfdc5930a2
Max Filippov <jcmvbkbc@gmail.com>
2017-05-30 11:02:35 -0700

    Update copyright/license information
    License all code as MIT, kernel module as dual MIT/GPL.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- xrp-kernel/xvp_main.c
+++ xrp-kernel/xvp_main.c
@@ -1,28 +1,9 @@
 /*
- * XVP - Linux device driver for APK.
- * $Id$
+ * XRP: Linux device driver for Xtensa Remote Processing
  *
- * Copyright (c) 2015 Cadence Design Systems, Inc.
+ * Copyright (c) 2015 - 2017 Cadence Design Systems, Inc.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of version 2 of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it would be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * Further, this software is distributed without any warranty that it
- * is free of the rightful claim of any third person regarding
- * infringement  or the like.  Any license provided herein, whether
- * implied or otherwise, applies only to this software file.  Patent
- * licenses, if any, provided herein do not apply to combinations of
- * this program with other software, or any other product whatsoever.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write the Free Software Foundation,
- * Inc., 59 Temple Place - Suite 330, Boston MA 02111-1307, USA.
- *
+ * License: Dual MIT/GPL.
  */
 
 #include <linux/completion.h>
@@ -2269,4 +2250,4 @@ module_platform_driver(xvp_driver);
 MODULE_AUTHOR("Takayuki Sugawara");
 MODULE_AUTHOR("Max Filippov");
 MODULE_DESCRIPTION("XRP: Linux device driver for Xtensa Remote Processing");
-MODULE_LICENSE("GPL v2");
+MODULE_LICENSE("Dual MIT/GPL");
