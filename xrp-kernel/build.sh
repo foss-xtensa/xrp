9efc3cdb10fb76c1dd019472720a08c646892d69
Max Filippov <jcmvbkbc@gmail.com>
2017-06-27 20:58:22 -0700

    xrp-kernel: drop build.sh script
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- xrp-kernel/build.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-ARCH=xtensa CROSS_COMPILE=/home/jcmvbkbc/tmp/br/build-xvp/host/usr/bin/xtensa-linux- KSRC=/home/jcmvbkbc/ws/tensilica/linux/xtensa-linux-test/builds/xvprel-sim-dc233c make "$@"
