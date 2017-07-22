foss-xtensa/xrp

commit 8d6098f8918c4e17a9b6ce277e4c67fb85a45028

xrp-kernel: don't load firmware when firmware-name is empty
There may be situations when the firmware is loaded into DSP externally,
e.g. by U-Boot, or the DSPs are in external simulator. The kernel may
not be able to load the firmware. Treat missing firmware-name property
as a request to not load it.

Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

xrp-kernel/xvp_main.c
@@ -30,9 +30,6 @@
 #include "xrp_kernel_defs.h"
 #include "xrp_kernel_dsp_interface.h"
 
-#define DEFAULT_FIRMWARE_NAME "xvp.elf"
-MODULE_FIRMWARE(DEFAULT_FIRMWARE_NAME);
-
 #define XVP_TIMEOUT_JIFFIES (HZ * 10)
 
 #define XVP_REG_RESET		(0x00)
@@ -1803,6 +1800,9 @@ static int xvp_boot_firmware(struct xvp *xvp)
 	int ret;
 	struct xrp_dsp_sync __iomem *shared_sync = xvp->comm;
 
+	if (!xvp->firmware_name)
+		return 0;
+
 	if (loopback < LOOPBACK_NOFIRMWARE) {
 		ret = xvp_request_firmware(xvp);
 		if (ret < 0)
@@ -1943,10 +1943,8 @@ static int xvp_probe(struct platform_device *pdev)
 	ret = of_property_read_string(pdev->dev.of_node, "firmware-name",
 				      &xvp->firmware_name);
 	if (ret == -EINVAL) {
-		xvp->firmware_name = DEFAULT_FIRMWARE_NAME;
 		dev_dbg(xvp->dev,
-			"no firmware-name property, defaulting to \"%s\"",
-			xvp->firmware_name);
+			"no firmware-name property, not loading firmware");
 	} else if (ret < 0) {
 		dev_err(xvp->dev, "invalid firmware name (%d)", ret);
 		goto err_free;

commit 847f8821fafd2b4c63498ec27dcce4627ea27841

xrp-kernel: always synchronize with firmware
Perform synchronization even if firmware image isn't getting loaded,
otherwise there's no way to communicate with the firmware.

Fixes: 8d6098f8918c (xrp-kernel: don't load firmware when firmware-name
is empty)

Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

xrp-kernel/xvp_main.c
@@ -1800,21 +1800,19 @@ static int xvp_boot_firmware(struct xvp *xvp)
 	int ret;
 	struct xrp_dsp_sync __iomem *shared_sync = xvp->comm;
 
-	if (!xvp->firmware_name)
-		return 0;
+	if (xvp->firmware_name) {
+		if (loopback < LOOPBACK_NOFIRMWARE) {
+			ret = xvp_request_firmware(xvp);
+			if (ret < 0)
+				return ret;
+		}
 
-	if (loopback < LOOPBACK_NOFIRMWARE) {
-		ret = xvp_request_firmware(xvp);
-		if (ret < 0)
-			return ret;
+		if (loopback < LOOPBACK_NOIO) {
+			xrp_comm_write32(&shared_sync->sync, XRP_DSP_SYNC_IDLE);
+			mb();
+		}
+		xvp_release_dsp(xvp);
 	}
-
-	if (loopback < LOOPBACK_NOIO) {
-		xrp_comm_write32(&shared_sync->sync, XRP_DSP_SYNC_IDLE);
-		mb();
-	}
-	xvp_release_dsp(xvp);
-
 	if (loopback < LOOPBACK_NOIO) {
 		ret = xvp_synchronize(xvp);
 		if (ret < 0) {