7a4409895188df27138cda52541bb93ca21c7705
Max Filippov <jcmvbkbc@gmail.com>
2017-08-14 15:41:07 -0700

    xrp-kernel: drop xrp_ioctl_wait definition
    Both IOCTL code and structure are unused now, drop them.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- xrp-kernel/xrp_kernel_defs.h
+++ xrp-kernel/xrp_kernel_defs.h
@@ -5,7 +5,6 @@
 #define XRP_IOCTL_ALLOC		_IO(XRP_IOCTL_MAGIC, 1)
 #define XRP_IOCTL_FREE		_IO(XRP_IOCTL_MAGIC, 2)
 #define XRP_IOCTL_QUEUE		_IO(XRP_IOCTL_MAGIC, 3)
-#define XRP_IOCTL_WAIT		_IO(XRP_IOCTL_MAGIC, 4)
 
 struct xrp_ioctl_alloc {
 	__u32 size;
@@ -33,10 +32,6 @@ struct xrp_ioctl_queue {
 	__u64 in_data_addr;
 	__u64 out_data_addr;
 	__u64 buffer_addr;
-};
-
-struct xrp_ioctl_wait {
-	__u32 cookie;
 };
 
 #endif
