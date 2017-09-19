foss-xtensa/xrp

commit 562b8c82953a02e2a5be0e03af0c7eb742b9166b

xrp-dsp: separate hw-specific part of the XRP
The linux XRP driver is now split into generic and HW-specific parts,
split the DSP XRP code accordingly. That provides clear interface for
porting XRP to a different hardware.
Introduce xrp_dsp_hw.h header with declarations of functions that the
HW-specific code needs to implement.

Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

xrp-dsp/xrp_dsp_hw_simple.c
@@ -1,0 +1,137 @@
+/*
+ * Copyright (c) 2016 - 2017 Cadence Design Systems Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <xtensa/tie/xt_interrupt.h>
+#include <xtensa/tie/xt_sync.h>
+#include <xtensa/xtruntime.h>
+
+#include "xrp_dsp_hw.h"
+
+typedef uint32_t __u32;
+#include "xrp_hw_simple_dsp_interface.h"
+
+#ifdef DEBUG
+#define pr_debug printf
+#else
+static inline int pr_debug(const char *p, ...)
+{
+	(void)p;
+	return 0;
+}
+#endif
+
+static uint32_t mmio_base;
+
+#define device_mmio(off) ((volatile void *)mmio_base + off)
+#define host_mmio(off) ((volatile void *)mmio_base + off)
+
+enum xrp_irq_mode {
+	XRP_IRQ_NONE,
+	XRP_IRQ_LEVEL,
+	XRP_IRQ_EDGE,
+};
+static enum xrp_irq_mode host_irq_mode;
+static enum xrp_irq_mode device_irq_mode;
+
+static uint32_t device_irq_offset;
+static uint32_t device_irq_bit;
+static uint32_t device_irq;
+
+static uint32_t host_irq_offset;
+static uint32_t host_irq_bit;
+
+
+static void xrp_irq_handler(void)
+{
+	pr_debug("%s\n", __func__);
+	if (device_irq_mode == XRP_IRQ_LEVEL)
+		XT_S32RI(0, device_mmio(device_irq_offset), 0);
+}
+
+void xrp_hw_send_host_irq(void)
+{
+	switch (host_irq_mode) {
+	case XRP_IRQ_EDGE:
+		XT_S32RI(0, host_mmio(host_irq_offset), 0);
+		/* fall through */
+	case XRP_IRQ_LEVEL:
+		XT_S32RI(1u << host_irq_bit, host_mmio(host_irq_offset), 0);
+		break;
+	default:
+		break;
+	}
+}
+
+void xrp_hw_wait_device_irq(void)
+{
+	if (device_irq_mode == XRP_IRQ_NONE)
+		return;
+
+	pr_debug("%s: waiting for device IRQ...\n", __func__);
+	_xtos_ints_on(1u << device_irq);
+	XT_WAITI(0);
+	_xtos_ints_off(1u << device_irq);
+}
+
+void xrp_hw_set_sync_data(void *p)
+{
+	static const enum xrp_irq_mode irq_mode[] = {
+		[XRP_DSP_SYNC_IRQ_MODE_NONE] = XRP_IRQ_NONE,
+		[XRP_DSP_SYNC_IRQ_MODE_LEVEL] = XRP_IRQ_LEVEL,
+		[XRP_DSP_SYNC_IRQ_MODE_EDGE] = XRP_IRQ_EDGE,
+	};
+	struct xrp_hw_simple_sync_data *hw_sync = p;
+
+	mmio_base = hw_sync->device_mmio_base;
+	pr_debug("%s: mmio_base: 0x%08x\n", __func__, mmio_base);
+
+	if (hw_sync->device_irq_mode < sizeof(irq_mode) / sizeof(*irq_mode)) {
+		device_irq_mode = irq_mode[hw_sync->device_irq_mode];
+		device_irq_offset = hw_sync->device_irq_offset;
+		device_irq_bit = hw_sync->device_irq_bit;
+		device_irq = hw_sync->device_irq;
+		pr_debug("%s: device_irq_mode = %d, device_irq_offset = %d, device_irq_bit = %d, device_irq = %d\n",
+			__func__, device_irq_mode,
+			device_irq_offset, device_irq_bit, device_irq);
+	} else {
+		device_irq_mode = XRP_IRQ_NONE;
+	}
+
+	if (hw_sync->host_irq_mode < sizeof(irq_mode) / sizeof(*irq_mode)) {
+		host_irq_mode = irq_mode[hw_sync->host_irq_mode];
+		host_irq_offset = hw_sync->host_irq_offset;
+		host_irq_bit = hw_sync->host_irq_bit;
+		pr_debug("%s: host_irq_mode = %d, host_irq_offset = %d, host_irq_bit = %d\n",
+			__func__, host_irq_mode, host_irq_offset, host_irq_bit);
+	} else {
+		host_irq_mode = XRP_IRQ_NONE;
+	}
+
+	if (device_irq_mode != XRP_IRQ_NONE) {
+		_xtos_ints_off(1u << device_irq);
+		_xtos_set_interrupt_handler(device_irq, xrp_irq_handler);
+		_xtos_dispatch_level1_interrupts();
+	}
+}

xrp-dsp/Makefile.am
@@ -1,7 +1,8 @@
 AM_CPPFLAGS = -I$(srcdir)/.. -I$(srcdir)/../xrp-kernel
 AM_CFLAGS = -W -Wall --xtensa-core=$(DSP_CORE)
 
-include_HEADERS = ../xrp_api.h
-lib_LIBRARIES = libxrp-dsp.a
+include_HEADERS = ../xrp_api.h xrp_dsp_hw.h
+lib_LIBRARIES = libxrp-dsp.a libxrp-dsp-hw-simple.a
 
 libxrp_dsp_a_SOURCES = xrp_dsp.c
+libxrp_dsp_hw_simple_a_SOURCES = xrp_dsp_hw_simple.c

xrp-dsp/Makefile.in
@@ -122,6 +122,10 @@ AM_V_AR = $(am__v_AR_@AM_V@)
 am__v_AR_ = $(am__v_AR_@AM_DEFAULT_V@)
 am__v_AR_0 = @echo "  AR      " $@;
 am__v_AR_1 = 
+libxrp_dsp_hw_simple_a_AR = $(AR) $(ARFLAGS)
+libxrp_dsp_hw_simple_a_LIBADD =
+am_libxrp_dsp_hw_simple_a_OBJECTS = xrp_dsp_hw_simple.$(OBJEXT)
+libxrp_dsp_hw_simple_a_OBJECTS = $(am_libxrp_dsp_hw_simple_a_OBJECTS)
 libxrp_dsp_a_AR = $(AR) $(ARFLAGS)
 libxrp_dsp_a_LIBADD =
 am_libxrp_dsp_a_OBJECTS = xrp_dsp.$(OBJEXT)
@@ -154,8 +158,9 @@ AM_V_CCLD = $(am__v_CCLD_@AM_V@)
 am__v_CCLD_ = $(am__v_CCLD_@AM_DEFAULT_V@)
 am__v_CCLD_0 = @echo "  CCLD    " $@;
 am__v_CCLD_1 = 
-SOURCES = $(libxrp_dsp_a_SOURCES)
-DIST_SOURCES = $(libxrp_dsp_a_SOURCES)
+SOURCES = $(libxrp_dsp_hw_simple_a_SOURCES) $(libxrp_dsp_a_SOURCES)
+DIST_SOURCES = $(libxrp_dsp_hw_simple_a_SOURCES) \
+	$(libxrp_dsp_a_SOURCES)
 am__can_run_installinfo = \
   case $$AM_UPDATE_INFO_DIR in \
     n|no|NO) false;; \
@@ -279,9 +284,10 @@ top_builddir = @top_builddir@
 top_srcdir = @top_srcdir@
 AM_CPPFLAGS = -I$(srcdir)/.. -I$(srcdir)/../xrp-kernel
 AM_CFLAGS = -W -Wall --xtensa-core=$(DSP_CORE)
-include_HEADERS = ../xrp_api.h
-lib_LIBRARIES = libxrp-dsp.a
+include_HEADERS = ../xrp_api.h xrp_dsp_hw.h
+lib_LIBRARIES = libxrp-dsp.a libxrp-dsp-hw-simple.a
 libxrp_dsp_a_SOURCES = xrp_dsp.c
+libxrp_dsp_hw_simple_a_SOURCES = xrp_dsp_hw_simple.c
 all: all-am
 
 .SUFFIXES:
@@ -348,6 +354,11 @@ uninstall-libLIBRARIES:
 clean-libLIBRARIES:
 	-test -z "$(lib_LIBRARIES)" || rm -f $(lib_LIBRARIES)
 
+libxrp-dsp-hw-simple.a: $(libxrp_dsp_hw_simple_a_OBJECTS) $(libxrp_dsp_hw_simple_a_DEPENDENCIES) $(EXTRA_libxrp_dsp_hw_simple_a_DEPENDENCIES) 
+	$(AM_V_at)-rm -f libxrp-dsp-hw-simple.a
+	$(AM_V_AR)$(libxrp_dsp_hw_simple_a_AR) libxrp-dsp-hw-simple.a $(libxrp_dsp_hw_simple_a_OBJECTS) $(libxrp_dsp_hw_simple_a_LIBADD)
+	$(AM_V_at)$(RANLIB) libxrp-dsp-hw-simple.a
+
 libxrp-dsp.a: $(libxrp_dsp_a_OBJECTS) $(libxrp_dsp_a_DEPENDENCIES) $(EXTRA_libxrp_dsp_a_DEPENDENCIES) 
 	$(AM_V_at)-rm -f libxrp-dsp.a
 	$(AM_V_AR)$(libxrp_dsp_a_AR) libxrp-dsp.a $(libxrp_dsp_a_OBJECTS) $(libxrp_dsp_a_LIBADD)
@@ -360,6 +371,7 @@ distclean-compile:
 	-rm -f *.tab.c
 
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/xrp_dsp.Po@am__quote@
+@AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/xrp_dsp_hw_simple.Po@am__quote@
 
 .c.o:
 @am__fastdepCC_TRUE@	$(AM_V_CC)depbase=`echo $@ | sed 's|[^/]*$$|$(DEPDIR)/&|;s|\.o$$||'`;\

xrp-dsp/xrp_dsp.c
@@ -23,16 +23,15 @@
 
 #include <stdint.h>
 #include <stdio.h>
-#include <xtensa/tie/xt_interrupt.h>
 #include <xtensa/tie/xt_sync.h>
 #include <xtensa/xtruntime.h>
 
 #include "xrp_api.h"
+#include "xrp_dsp_hw.h"
 
 typedef uint8_t __u8;
 typedef uint32_t __u32;
 #include "xrp_kernel_dsp_interface.h"
-#include "xrp_hw_simple_dsp_interface.h"
 
 #ifdef DEBUG
 #define pr_debug printf
@@ -47,27 +46,7 @@ static inline int pr_debug(const char *p, ...)
 extern char xrp_dsp_comm_base_magic[] __attribute__((weak));
 void *xrp_dsp_comm_base = &xrp_dsp_comm_base_magic;
 
-static uint32_t mmio_base;
-
-enum xrp_irq_mode {
-	XRP_IRQ_NONE,
-	XRP_IRQ_LEVEL,
-	XRP_IRQ_EDGE,
-};
-static enum xrp_irq_mode host_irq_mode;
-static enum xrp_irq_mode device_irq_mode;
-
-static uint32_t device_irq_offset;
-static uint32_t device_irq_bit;
-static uint32_t device_irq;
-
-static uint32_t host_irq_offset;
-static uint32_t host_irq_bit;
-
 static int manage_cache;
-
-#define device_mmio(off) ((volatile void *)mmio_base + off)
-#define host_mmio(off) ((volatile void *)mmio_base + off)
 
 /* DSP side XRP API implementation */
 
@@ -221,38 +200,9 @@ struct xrp_buffer *xrp_get_buffer_from_group(struct xrp_buffer_group *group,
 
 /* DSP side request handling */
 
-static void xrp_irq_handler(void)
-{
-	pr_debug("%s\n", __func__);
-	if (device_irq_mode == XRP_IRQ_LEVEL)
-		XT_S32RI(0, device_mmio(device_irq_offset), 0);
-}
-
-static void xrp_send_host_irq(void)
-{
-	switch (host_irq_mode) {
-	case XRP_IRQ_EDGE:
-		XT_S32RI(0, host_mmio(host_irq_offset), 0);
-		/* fall through */
-	case XRP_IRQ_LEVEL:
-		XT_S32RI(1u << host_irq_bit, host_mmio(host_irq_offset), 0);
-		break;
-	default:
-		break;
-	}
-}
-
 static void do_handshake(struct xrp_dsp_sync *shared_sync)
 {
 	uint32_t v;
-	static const enum xrp_irq_mode irq_mode[] = {
-		[XRP_DSP_SYNC_IRQ_MODE_NONE] = XRP_IRQ_NONE,
-		[XRP_DSP_SYNC_IRQ_MODE_LEVEL] = XRP_IRQ_LEVEL,
-		[XRP_DSP_SYNC_IRQ_MODE_EDGE] = XRP_IRQ_EDGE,
-	};
-	struct xrp_hw_simple_sync_data *hw_sync =
-		(struct xrp_hw_simple_sync_data *)&shared_sync->hw_sync_data;
-
 
 	pr_debug("%s, shared_sync = %p\n", __func__, shared_sync);
 start:
@@ -275,52 +225,15 @@ start:
 			goto start;
 	}
 
-	mmio_base = hw_sync->device_mmio_base;
-	pr_debug("%s: mmio_base: 0x%08x\n", __func__, mmio_base);
+	xrp_hw_set_sync_data(shared_sync->hw_sync_data);
 
-	if (hw_sync->device_irq_mode < sizeof(irq_mode) / sizeof(*irq_mode)) {
-		device_irq_mode = irq_mode[hw_sync->device_irq_mode];
-		device_irq_offset = hw_sync->device_irq_offset;
-		device_irq_bit = hw_sync->device_irq_bit;
-		device_irq = hw_sync->device_irq;
-		pr_debug("%s: device_irq_mode = %d, device_irq_offset = %d, device_irq_bit = %d, device_irq = %d\n",
-			__func__, device_irq_mode,
-			device_irq_offset, device_irq_bit, device_irq);
-	} else {
-		device_irq_mode = XRP_IRQ_NONE;
-	}
+	XT_S32RI(XRP_DSP_SYNC_DSP_TO_HOST, &shared_sync->sync, 0);
+	dcache_region_writeback(&shared_sync->sync,
+				sizeof(shared_sync->sync));
 
-	if (hw_sync->host_irq_mode < sizeof(irq_mode) / sizeof(*irq_mode)) {
-		host_irq_mode = irq_mode[hw_sync->host_irq_mode];
-		host_irq_offset = hw_sync->host_irq_offset;
-		host_irq_bit = hw_sync->host_irq_bit;
-		pr_debug("%s: host_irq_mode = %d, host_irq_offset = %d, host_irq_bit = %d\n",
-			__func__, host_irq_mode, host_irq_offset, host_irq_bit);
-	} else {
-		host_irq_mode = XRP_IRQ_NONE;
-	}
+	xrp_hw_wait_device_irq();
 
-	if (device_irq_mode != XRP_IRQ_NONE) {
-		_xtos_ints_off(1u << device_irq);
-		_xtos_set_interrupt_handler(device_irq, xrp_irq_handler);
-		_xtos_dispatch_level1_interrupts();
-		XTOS_SET_INTLEVEL(15);
-
-		XT_S32RI(XRP_DSP_SYNC_DSP_TO_HOST, &shared_sync->sync, 0);
-		dcache_region_writeback(&shared_sync->sync,
-					sizeof(shared_sync->sync));
-
-		pr_debug("%s: waiting for device IRQ...\n", __func__);
-		_xtos_ints_on(1u << device_irq);
-		XT_WAITI(0);
-		XTOS_SET_INTLEVEL(15);
-		_xtos_ints_off(1u << device_irq);
-	} else {
-		XT_S32RI(XRP_DSP_SYNC_DSP_TO_HOST, &shared_sync->sync, 0);
-		dcache_region_writeback(&shared_sync->sync,
-					sizeof(shared_sync->sync));
-	}
-	xrp_send_host_irq();
+	xrp_hw_send_host_irq();
 
 	pr_debug("%s: done\n", __func__);
 }
@@ -340,31 +253,12 @@ static inline int xrp_request_valid(struct xrp_dsp_cmd *dsp_cmd,
 static void wait_for_request(struct xrp_dsp_cmd *dsp_cmd,
 			     uint32_t *pflags)
 {
-	if (device_irq_mode != XRP_IRQ_NONE) {
-		unsigned level = XTOS_SET_INTLEVEL(15);
-
-		for (;;) {
-			if (device_irq_mode == XRP_IRQ_LEVEL)
-				XT_S32RI(0, device_mmio(device_irq_offset), 0);
-
-			dcache_region_invalidate(dsp_cmd,
-						 sizeof(*dsp_cmd));
-			if (xrp_request_valid(dsp_cmd, pflags))
-				break;
-
-			_xtos_ints_on(1u << device_irq);
-			XT_WAITI(0);
-			XTOS_SET_INTLEVEL(15);
-			_xtos_ints_off(1u << device_irq);
-		}
-		XTOS_RESTORE_INTLEVEL(level);
-	} else {
-		for (;;) {
-			dcache_region_invalidate(dsp_cmd,
-						 sizeof(*dsp_cmd));
-			if (xrp_request_valid(dsp_cmd, pflags))
-				break;
-		}
+	for (;;) {
+		dcache_region_invalidate(dsp_cmd,
+					 sizeof(*dsp_cmd));
+		if (xrp_request_valid(dsp_cmd, pflags))
+			break;
+		xrp_hw_wait_device_irq();
 	}
 }
 
@@ -377,7 +271,7 @@ static void complete_request(struct xrp_dsp_cmd *dsp_cmd)
 	XT_S32RI(flags, &dsp_cmd->flags, 0);
 	dcache_region_writeback(&dsp_cmd->flags,
 				sizeof(dsp_cmd->flags));
-	xrp_send_host_irq();
+	xrp_hw_send_host_irq();
 }
 
 static enum xrp_access_flags dsp_buffer_allowed_access(__u32 flags)

xrp-dsp/xrp_dsp_hw.h
@@ -1,0 +1,31 @@
+/*
+ * Copyright (c) 2017 Cadence Design Systems Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef XRP_DSP_HW_H
+#define XRP_DSP_HW_H
+
+void xrp_hw_send_host_irq(void);
+void xrp_hw_wait_device_irq(void);
+void xrp_hw_set_sync_data(void *p);
+
+#endif

xrp-example/Makefile.am
@@ -34,8 +34,8 @@ endif
 xrp_dsp_nat_SOURCES = dsp_main.c
 xrp_dsp_sim_SOURCES = dsp_main.c
 
-xrp_dsp_nat_LDADD = ../xrp-dsp/libxrp-dsp.a
-xrp_dsp_sim_LDADD = ../xrp-dsp/libxrp-dsp.a
+xrp_dsp_nat_LDADD = ../xrp-dsp/libxrp-dsp.a ../xrp-dsp/libxrp-dsp-hw-simple.a
+xrp_dsp_sim_LDADD = ../xrp-dsp/libxrp-dsp.a ../xrp-dsp/libxrp-dsp-hw-simple.a
 
 xrp_linux_nat_SOURCES = linux_main.c
 xrp_linux_sim_SOURCES = linux_main.c xrp.s

xrp-example/Makefile.in
@@ -103,12 +103,14 @@ am__installdirs = "$(DESTDIR)$(bindir)"
 PROGRAMS = $(bin_PROGRAMS)
 am_xrp_dsp_nat_OBJECTS = dsp_main.$(OBJEXT)
 xrp_dsp_nat_OBJECTS = $(am_xrp_dsp_nat_OBJECTS)
-xrp_dsp_nat_DEPENDENCIES = ../xrp-dsp/libxrp-dsp.a
+xrp_dsp_nat_DEPENDENCIES = ../xrp-dsp/libxrp-dsp.a \
+	../xrp-dsp/libxrp-dsp-hw-simple.a
 xrp_dsp_nat_LINK = $(CCLD) $(AM_CFLAGS) $(CFLAGS) \
 	$(xrp_dsp_nat_LDFLAGS) $(LDFLAGS) -o $@
 am_xrp_dsp_sim_OBJECTS = dsp_main.$(OBJEXT)
 xrp_dsp_sim_OBJECTS = $(am_xrp_dsp_sim_OBJECTS)
-xrp_dsp_sim_DEPENDENCIES = ../xrp-dsp/libxrp-dsp.a
+xrp_dsp_sim_DEPENDENCIES = ../xrp-dsp/libxrp-dsp.a \
+	../xrp-dsp/libxrp-dsp-hw-simple.a
 xrp_dsp_sim_LINK = $(CCLD) $(AM_CFLAGS) $(CFLAGS) \
 	$(xrp_dsp_sim_LDFLAGS) $(LDFLAGS) -o $@
 am_xrp_linux_nat_OBJECTS = linux_main.$(OBJEXT)
@@ -297,8 +299,8 @@ AM_CFLAGS = -W -Wall $(am__append_1)
 @BUILD_DSP_FALSE@@BUILD_SIM_TRUE@CLEANFILES = xrp.s
 xrp_dsp_nat_SOURCES = dsp_main.c
 xrp_dsp_sim_SOURCES = dsp_main.c
-xrp_dsp_nat_LDADD = ../xrp-dsp/libxrp-dsp.a
-xrp_dsp_sim_LDADD = ../xrp-dsp/libxrp-dsp.a
+xrp_dsp_nat_LDADD = ../xrp-dsp/libxrp-dsp.a ../xrp-dsp/libxrp-dsp-hw-simple.a
+xrp_dsp_sim_LDADD = ../xrp-dsp/libxrp-dsp.a ../xrp-dsp/libxrp-dsp-hw-simple.a
 xrp_linux_nat_SOURCES = linux_main.c
 xrp_linux_sim_SOURCES = linux_main.c xrp.s
 xrp_linux_sim_CFLAGS = -pthread

commit 2b0025be8fa400a645619aef3543ebb8979dd7e0

xrp-dsp: avoid race in xrp_hw_wait_device_irq
Since xrp_hw_wait_device_irq can be called with any intlevel and DSP IRQ
may already be posted it is possible that IRQ handler will be called
before the WAITI instruction, resulting in a subsequent hang and command
timeout on the host side. Avoid that race by raising intlevel to 15 just
before enabling DSP IRQ. Restore the original intlevel after DSP IRQ is
disabled.

Fixes: 562b8c82953a ("xrp-dsp: separate hw-specific part of the XRP")
Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

xrp-dsp/xrp_dsp_hw_simple.c
@@ -86,13 +86,17 @@ void xrp_hw_send_host_irq(void)
 
 void xrp_hw_wait_device_irq(void)
 {
+	unsigned old_intlevel;
+
 	if (device_irq_mode == XRP_IRQ_NONE)
 		return;
 
 	pr_debug("%s: waiting for device IRQ...\n", __func__);
+	old_intlevel = XTOS_SET_INTLEVEL(15);
 	_xtos_ints_on(1u << device_irq);
 	XT_WAITI(0);
 	_xtos_ints_off(1u << device_irq);
+	XTOS_RESTORE_INTLEVEL(old_intlevel);
 }
 
 void xrp_hw_set_sync_data(void *p)