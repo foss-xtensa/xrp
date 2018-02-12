foss-xtensa/xrp

commit cd0fb2a5720696395cd032a354518d7e9fd0e81f

xrp-kernel: xrp-linux-sim: add device-irq-host-offset property
In fast simulation it may be desirable to have device IRQ register at
different offsets for the host and for the DSP. Introduce optional
property device-irq-host-offset used by the host to find location of the
device IRQ register. If not specified the first cell of device-irq is
used.

Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

xrp-kernel/xrp_hw_simple.c
@@ -59,11 +59,13 @@ struct xrp_hw_simple {
 	/* how IRQ is used to notify the device of incoming data */
 	enum xrp_irq_mode device_irq_mode;
 	/*
-	 * offset of IRQ register in MMIO region (host side)
+	 * offset of device IRQ register in MMIO region (device side)
 	 * bit number
 	 * device IRQ#
 	 */
 	u32 device_irq[3];
+	/* offset of devuce IRQ register in MMIO region (host side) */
+	u32 device_irq_host_offset;
 	/* how IRQ is used to notify the host of incoming data */
 	enum xrp_irq_mode host_irq_mode;
 	/*
@@ -125,11 +127,11 @@ static void send_irq(void *hw_arg)
 
 	switch (hw->device_irq_mode) {
 	case XRP_IRQ_EDGE:
-		reg_write32(hw, hw->device_irq[0], 0);
+		reg_write32(hw, hw->device_irq_host_offset, 0);
 		/* fallthrough */
 	case XRP_IRQ_LEVEL:
 		wmb();
-		reg_write32(hw, hw->device_irq[0],
+		reg_write32(hw, hw->device_irq_host_offset,
 			    BIT(hw->device_irq[1]));
 		break;
 	default:
@@ -241,6 +243,19 @@ static int init_hw(struct platform_device *pdev, struct xrp_hw_simple *hw,
 					 hw->device_irq,
 					 ARRAY_SIZE(hw->device_irq));
 	if (ret == 0) {
+		u32 device_irq_host_offset;
+
+		ret = of_property_read_u32(pdev->dev.of_node,
+					   "device-irq-host-offset",
+					   &device_irq_host_offset);
+		if (ret == 0) {
+			hw->device_irq_host_offset = &device_irq_host_offset;
+		} else {
+			hw->device_irq_host_offset = hw->device_irq[0];
+			ret = 0;
+		}
+	}
+	if (ret == 0) {
 		u32 device_irq_mode;
 
 		ret = of_property_read_u32(pdev->dev.of_node,
@@ -253,8 +268,9 @@ static int init_hw(struct platform_device *pdev, struct xrp_hw_simple *hw,
 	}
 	if (ret == 0) {
 		dev_dbg(&pdev->dev,
-			"%s: device IRQ MMIO offset = 0x%08x, bit = %d, device IRQ = %d, IRQ mode = %d",
-			__func__, hw->device_irq[0], hw->device_irq[1],
+			"%s: device IRQ MMIO host offset = 0x%08x, offset = 0x%08x, bit = %d, device IRQ = %d, IRQ mode = %d",
+			__func__, hw->device_irq_host_offset,
+			hw->device_irq[0], hw->device_irq[1],
 			hw->device_irq[2], hw->device_irq_mode);
 	} else {
 		dev_info(&pdev->dev,

xrp-linux-sim/xrp_linux_sim.c
@@ -97,6 +97,7 @@ struct xrp_device_description {
 
 	uint32_t device_irq_mode;
 	uint32_t device_irq[3];
+	uint32_t device_irq_host_offset;
 	pthread_mutex_t hw_mutex;
 };
 
@@ -384,7 +385,7 @@ static void initialize_shmem(void)
 
 static inline void xrp_send_device_irq(struct xrp_device_description *desc)
 {
-	void *device_irq = p2v(desc->io_base + desc->device_irq[0]);
+	void *device_irq = p2v(desc->io_base + desc->device_irq_host_offset);
 
 	switch (desc->device_irq_mode) {
 	case XRP_IRQ_EDGE:
@@ -548,6 +549,7 @@ static int init_cdns_xrp_hw_simple_common(void *fdt, int offset,
 					  struct xrp_device_description *description)
 {
 	const void *device_irq;
+	const void *device_irq_host_offset;
 	const void *device_irq_mode;
 	int len;
 
@@ -573,6 +575,19 @@ static int init_cdns_xrp_hw_simple_common(void *fdt, int offset,
 				getprop_u32(device_irq, 4);
 			description->device_irq[2] =
 				getprop_u32(device_irq, 8);
+
+			device_irq_host_offset = fdt_getprop(fdt, offset,
+							     "device-irq-host-offset",
+							     &len);
+			if (!device_irq_host_offset || len < 4) {
+				description->device_irq_host_offset =
+					description->device_irq[0];
+				printf("%s: valid device-irq-host-offset not found, not using\n",
+				       __func__);
+			} else {
+				description->device_irq_host_offset =
+					getprop_u32(device_irq_host_offset, 0);
+			}
 		}
 	}
 	return 1;

commit ea7ff8a9088efba0dec9ca4bff1aa2d508228a84

xrp-kernel: fix device_irq_host_offset initialization
Fixes: cd0fb2a57206 ("xrp-kernel: xrp-linux-sim: add device-irq-host-offset property")
Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

xrp-kernel/xrp_hw_simple.c
@@ -249,7 +249,7 @@ static int init_hw(struct platform_device *pdev, struct xrp_hw_simple *hw,
 					   "device-irq-host-offset",
 					   &device_irq_host_offset);
 		if (ret == 0) {
-			hw->device_irq_host_offset = &device_irq_host_offset;
+			hw->device_irq_host_offset = device_irq_host_offset;
 		} else {
 			hw->device_irq_host_offset = hw->device_irq[0];
 			ret = 0;