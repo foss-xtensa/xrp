foss-xtensa/xrp

commit 138699f0b3a84a9ffd66be4765369a27b5c900a9

xrp-kernel: don't expose struct xvp to HW-specific driver
Limit interface between generic and HW-specific drivers to xrp_hw.h and
make struct xvp opaque to the HW-specific driver.

Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

xrp-kernel/xvp_main.c
@@ -1680,17 +1680,26 @@ static int xrp_init_regs_cma(struct platform_device *pdev, struct xvp *xvp)
 	return xrp_init_cma_pool(&xvp->pool, xvp->dev);
 }
 
-static int xrp_init_common(struct platform_device *pdev, struct xvp *xvp,
-			   const struct xrp_hw_ops *hw_ops, void *hw_arg,
-			   int (*xrp_init_regs)(struct platform_device *pdev,
-						struct xvp *xvp))
+static long xrp_init_common(struct platform_device *pdev,
+			    enum xrp_init_flags init_flags,
+			    const struct xrp_hw_ops *hw_ops, void *hw_arg,
+			    int (*xrp_init_regs)(struct platform_device *pdev,
+						 struct xvp *xvp))
 {
-	int ret;
+	long ret;
 	char nodename[sizeof("xvp") + 3 * sizeof(int)];
+	struct xvp *xvp = devm_kzalloc(&pdev->dev, sizeof(*xvp), GFP_KERNEL);
+
+	if (!xvp) {
+		ret = -ENOMEM;
+		goto err;
+	}
 
 	xvp->dev = &pdev->dev;
 	xvp->hw_ops = hw_ops;
 	xvp->hw_arg = hw_arg;
+	if (init_flags & XRP_INIT_USE_HOST_IRQ)
+		xvp->host_irq_mode = true;
 	platform_set_drvdata(pdev, xvp);
 	mutex_init(&xvp->comm_lock);
 	init_completion(&xvp->completion);
@@ -1712,7 +1721,7 @@ static int xrp_init_common(struct platform_device *pdev, struct xvp *xvp,
 		dev_dbg(xvp->dev,
 			"no firmware-name property, not loading firmware");
 	} else if (ret < 0) {
-		dev_err(xvp->dev, "invalid firmware name (%d)", ret);
+		dev_err(xvp->dev, "invalid firmware name (%ld)", ret);
 		goto err_free_map;
 	}
 
@@ -1735,7 +1744,7 @@ static int xrp_init_common(struct platform_device *pdev, struct xvp *xvp,
 	ret = misc_register(&xvp->miscdev);
 	if (ret < 0)
 		goto err_pm_disable;
-	return 0;
+	return PTR_ERR(xvp);
 err_pm_disable:
 	pm_runtime_disable(xvp->dev);
 err_free_map:
@@ -1747,28 +1756,28 @@ err_free_pool:
 			       phys_to_dma(xvp->dev, xvp->comm_phys), 0);
 	}
 err:
-	dev_err(&pdev->dev, "%s: ret = %d\n", __func__, ret);
+	dev_err(&pdev->dev, "%s: ret = %ld\n", __func__, ret);
 	return ret;
 }
 
-int xrp_init(struct platform_device *pdev, struct xvp *xvp,
-	     const struct xrp_hw_ops *hw_ops, void *hw_arg)
+long xrp_init(struct platform_device *pdev, enum xrp_init_flags flags,
+	      const struct xrp_hw_ops *hw_ops, void *hw_arg)
 {
-	return xrp_init_common(pdev, xvp, hw_ops, hw_arg, xrp_init_regs_v0);
+	return xrp_init_common(pdev, flags, hw_ops, hw_arg, xrp_init_regs_v0);
 }
 EXPORT_SYMBOL(xrp_init);
 
-int xrp_init_v1(struct platform_device *pdev, struct xvp *xvp,
-		const struct xrp_hw_ops *hw_ops, void *hw_arg)
+long xrp_init_v1(struct platform_device *pdev, enum xrp_init_flags flags,
+		 const struct xrp_hw_ops *hw_ops, void *hw_arg)
 {
-	return xrp_init_common(pdev, xvp, hw_ops, hw_arg, xrp_init_regs_v1);
+	return xrp_init_common(pdev, flags, hw_ops, hw_arg, xrp_init_regs_v1);
 }
 EXPORT_SYMBOL(xrp_init_v1);
 
-int xrp_init_cma(struct platform_device *pdev, struct xvp *xvp,
-		 const struct xrp_hw_ops *hw_ops, void *hw_arg)
+long xrp_init_cma(struct platform_device *pdev, enum xrp_init_flags flags,
+		  const struct xrp_hw_ops *hw_ops, void *hw_arg)
 {
-	return xrp_init_common(pdev, xvp, hw_ops, hw_arg, xrp_init_regs_cma);
+	return xrp_init_common(pdev, flags, hw_ops, hw_arg, xrp_init_regs_cma);
 }
 EXPORT_SYMBOL(xrp_init_cma);
 

xrp-kernel/xrp_hw.h
@@ -57,12 +57,16 @@ struct xrp_hw_ops {
 				 unsigned long sz);
 };
 
-int xrp_init(struct platform_device *pdev, struct xvp *xvp,
-	     const struct xrp_hw_ops *hw, void *hw_arg);
-int xrp_init_v1(struct platform_device *pdev, struct xvp *xvp,
-		const struct xrp_hw_ops *hw, void *hw_arg);
-int xrp_init_cma(struct platform_device *pdev, struct xvp *xvp,
+enum xrp_init_flags {
+	XRP_INIT_USE_HOST_IRQ = 0x1,
+};
+
+long xrp_init(struct platform_device *pdev, enum xrp_init_flags flags,
+	      const struct xrp_hw_ops *hw, void *hw_arg);
+long xrp_init_v1(struct platform_device *pdev, enum xrp_init_flags flags,
 		 const struct xrp_hw_ops *hw, void *hw_arg);
+long xrp_init_cma(struct platform_device *pdev, enum xrp_init_flags flags,
+		  const struct xrp_hw_ops *hw, void *hw_arg);
 
 int xrp_deinit(struct platform_device *pdev);
 irqreturn_t xrp_irq_handler(int irq, struct xvp *xvp);

xrp-kernel/xrp_hw_simple.c
@@ -37,7 +37,6 @@
 #include <asm/cacheflush.h>
 #include "xrp_hw.h"
 #include "xrp_hw_simple_dsp_interface.h"
-#include "xrp_internal.h"
 
 #define DRIVER_NAME "xrp-hw-simple"
 
@@ -52,7 +51,7 @@ enum xrp_irq_mode {
 };
 
 struct xrp_hw_simple {
-	struct xvp xrp;
+	struct xvp *xrp;
 	phys_addr_t regs_phys;
 	void __iomem *regs;
 
@@ -150,7 +149,7 @@ static void ack_irq(void *hw_arg)
 static irqreturn_t irq_handler(int irq, void *dev_id)
 {
 	struct xrp_hw_simple *hw = dev_id;
-	irqreturn_t ret = xrp_irq_handler(irq, &hw->xrp);
+	irqreturn_t ret = xrp_irq_handler(irq, hw->xrp);
 
 	if (ret == IRQ_HANDLED)
 		ack_irq(hw);
@@ -221,12 +220,12 @@ static const struct xrp_hw_ops hw_ops = {
 	.invalidate_cache = invalidate_cache,
 };
 
-static int init_hw(struct platform_device *pdev, struct xrp_hw_simple *hw,
-		   int mem_idx)
+static long init_hw(struct platform_device *pdev, struct xrp_hw_simple *hw,
+		    int mem_idx, enum xrp_init_flags *init_flags)
 {
 	struct resource *mem;
 	int irq;
-	int ret;
+	long ret;
 
 	mem = platform_get_resource(pdev, IORESOURCE_MEM, mem_idx);
 	if (!mem) {
@@ -301,7 +300,7 @@ static int init_hw(struct platform_device *pdev, struct xrp_hw_simple *hw,
 			dev_err(&pdev->dev, "request_irq %d failed\n", irq);
 			goto err;
 		}
-		hw->xrp.host_irq_mode = true;
+		*init_flags |= XRP_INIT_USE_HOST_IRQ;
 	} else {
 		dev_info(&pdev->dev, "using polling mode on the host side\n");
 	}
@@ -310,37 +309,40 @@ err:
 	return ret;
 }
 
-static int init(struct platform_device *pdev, struct xrp_hw_simple *hw)
+static long init(struct platform_device *pdev, struct xrp_hw_simple *hw)
 {
-	int ret;
+	long ret;
+	enum xrp_init_flags init_flags = 0;
 
-	ret = init_hw(pdev, hw, 0);
+	ret = init_hw(pdev, hw, 0, &init_flags);
 	if (ret < 0)
 		return ret;
 
-	return xrp_init(pdev, &hw->xrp, &hw_ops, hw);
+	return xrp_init(pdev, init_flags, &hw_ops, hw);
 }
 
-static int init_v1(struct platform_device *pdev, struct xrp_hw_simple *hw)
+static long init_v1(struct platform_device *pdev, struct xrp_hw_simple *hw)
 {
-	int ret;
+	long ret;
+	enum xrp_init_flags init_flags = 0;
 
-	ret = init_hw(pdev, hw, 1);
+	ret = init_hw(pdev, hw, 1, &init_flags);
 	if (ret < 0)
 		return ret;
 
-	return xrp_init_v1(pdev, &hw->xrp, &hw_ops, hw);
+	return xrp_init_v1(pdev, init_flags, &hw_ops, hw);
 }
 
-static int init_cma(struct platform_device *pdev, struct xrp_hw_simple *hw)
+static long init_cma(struct platform_device *pdev, struct xrp_hw_simple *hw)
 {
-	int ret;
+	long ret;
+	enum xrp_init_flags init_flags = 0;
 
-	ret = init_hw(pdev, hw, 0);
+	ret = init_hw(pdev, hw, 0, &init_flags);
 	if (ret < 0)
 		return ret;
 
-	return xrp_init_cma(pdev, &hw->xrp, &hw_ops, hw);
+	return xrp_init_cma(pdev, init_flags, &hw_ops, hw);
 }
 
 #ifdef CONFIG_OF
@@ -364,7 +366,8 @@ static int xrp_hw_simple_probe(struct platform_device *pdev)
 	struct xrp_hw_simple *hw =
 		devm_kzalloc(&pdev->dev, sizeof(*hw), GFP_KERNEL);
 	const struct of_device_id *match;
-	int (*init)(struct platform_device *pdev, struct xrp_hw_simple *hw);
+	long (*init)(struct platform_device *pdev, struct xrp_hw_simple *hw);
+	long ret;
 
 	if (!hw)
 		return -ENOMEM;
@@ -375,7 +378,14 @@ static int xrp_hw_simple_probe(struct platform_device *pdev)
 		return -ENODEV;
 
 	init = match->data;
-	return init(pdev, hw);
+	ret = init(pdev, hw);
+	if (IS_ERR_VALUE(ret)) {
+		xrp_deinit(pdev);
+		return ret;
+	} else {
+		hw->xrp = ERR_PTR(ret);
+		return 0;
+	}
 
 }
 

commit 9d52dda85f96b8c1bb017f820032b2b623350d91

xrp-kernel: fix xrp_probe calls to xrp_init*
These calls were not updated when prototype of the xrp_init* functions
changed, resulting in compilation error on x86 and runtime error on
OF-enabled targets.
Fixes: 138699f0b3a8 ("xrp-kernel: don't expose struct xvp to HW-specific
driver")

Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

xrp-kernel/xvp_main.c
@@ -1810,6 +1810,11 @@ err:
 	return ret;
 }
 
+typedef long xrp_init_function(struct platform_device *pdev,
+			       enum xrp_init_flags flags,
+			       const struct xrp_hw_ops *hw_ops, void *hw_arg);
+
+xrp_init_function xrp_init;
 long xrp_init(struct platform_device *pdev, enum xrp_init_flags flags,
 	      const struct xrp_hw_ops *hw_ops, void *hw_arg)
 {
@@ -1817,6 +1822,7 @@ long xrp_init(struct platform_device *pdev, enum xrp_init_flags flags,
 }
 EXPORT_SYMBOL(xrp_init);
 
+xrp_init_function xrp_init_v1;
 long xrp_init_v1(struct platform_device *pdev, enum xrp_init_flags flags,
 		 const struct xrp_hw_ops *hw_ops, void *hw_arg)
 {
@@ -1824,6 +1830,7 @@ long xrp_init_v1(struct platform_device *pdev, enum xrp_init_flags flags,
 }
 EXPORT_SYMBOL(xrp_init_v1);
 
+xrp_init_function xrp_init_cma;
 long xrp_init_cma(struct platform_device *pdev, enum xrp_init_flags flags,
 		  const struct xrp_hw_ops *hw_ops, void *hw_arg)
 {
@@ -1900,27 +1907,26 @@ MODULE_DEVICE_TABLE(acpi, xrp_acpi_match);
 
 static int xrp_probe(struct platform_device *pdev)
 {
-	int ret = -EINVAL;
-	struct xvp *xvp = devm_kzalloc(&pdev->dev, sizeof(*xvp), GFP_KERNEL);
-	if (!xvp)
-		return -ENOMEM;
+	long ret = -EINVAL;
 
 #ifdef CONFIG_OF
 	{
 		const struct of_device_id *match;
-		int (*init)(struct platform_device *pdev, struct xvp *xvp,
-			    const struct xrp_hw_ops *hw_ops, void *hw_arg);
+		xrp_init_function *init;
 
 	        match = of_match_device(xrp_of_match, &pdev->dev);
 		init = match->data;
-		return init(pdev, xvp, &hw_ops, NULL);
+		ret = init(pdev, 0, &hw_ops, NULL);
+		return IS_ERR_VALUE(ret) ? ret : 0;
 	}
 #endif
 #ifdef CONFIG_ACPI
-	ret = xrp_init_v1(pdev, xvp, &hw_ops, NULL);
-	if (ret == 0) {
+	ret = xrp_init_v1(pdev, 0, &hw_ops, NULL);
+	if (!IS_ERR_VALUE(ret)) {
 		struct xrp_address_map_entry *entry;
+		struct xvp *xvp = ERR_PTR(ret);
 
+		ret = 0;
 		/*
 		 * On ACPI system DSP can currently only access
 		 * its own shared memory.