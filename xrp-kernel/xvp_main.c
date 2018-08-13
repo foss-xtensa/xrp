foss-xtensa/xrp

commit 071fda1f07a4b2578c8725a9d9c906f7900a7d47

xrp-kernel: fix multiple issues in xvp_copy_virt_to_phys
- xvp_copy_virt_to_phys may write past the end of the allocated buffer if
  incoming vaddr is page-aligned, but allocated hadow buffer is not;
- xvp_copy_virt_to_phys incorrectly frees aligned pointer instead of the
  original allocation if copy_from_user fails;
- xvp_copy_virt_to_phys has hardcoded maximum alignment (PAGE_SIZE) that
  is used unconditionally, but instead it could use the actual alignemt
  of the vaddr clamped to a reasonable range.

Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

xrp-kernel/xvp_main.c
@@ -801,21 +801,21 @@ static long xvp_copy_virt_to_phys(struct xvp_file *xvp_file,
 				  struct xvp_alien_mapping **mapping)
 {
 	unsigned long phys;
-	unsigned long offset = vaddr & (PAGE_SIZE - 1);
-	void *allocation = kmalloc(size + (offset ? PAGE_SIZE : 0),
-				   GFP_KERNEL);
+	unsigned long align = clamp(vaddr & -vaddr, PAGE_SIZE, 16ul);
+	unsigned long offset = vaddr & (align - 1);
+	void *allocation = kmalloc(size + align, GFP_KERNEL);
 	void *p;
 	struct xvp_alien_mapping *alien_mapping;
 
 	if (!allocation)
 		return -ENOMEM;
 
-	p = (void *)((((unsigned long)allocation) & PAGE_MASK) | offset);
+	p = (void *)((((unsigned long)allocation) & -align) | offset);
 	if (p < allocation)
-		p += PAGE_SIZE;
+		p += align;
 
 	if (copy_from_user(p, (void __user *)vaddr, size)) {
-		kfree(p);
+		kfree(allocation);
 		return -EFAULT;
 	}
 

commit 82cc095487ac32c9afe09efe98a4779260a9a127

xrp-kernel: fix alignment logic
Kernel driver is supposed to preserve buffer alignment for shadow
copies, but the actual behavior didn't match documented logic: it has
been always preserving 4 least significant bits, even when buffers had
greater alignment. Fix that.

Fixes: 071fda1f07a4 ("xrp-kernel: fix multiple issues in
xvp_copy_virt_to_phys")
Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

xrp-kernel/xvp_main.c
@@ -660,7 +660,7 @@ static long xvp_copy_virt_to_phys(struct xvp_file *xvp_file,
 				  struct xrp_alien_mapping *mapping)
 {
 	phys_addr_t phys;
-	unsigned long align = clamp(vaddr & -vaddr, PAGE_SIZE, 16ul);
+	unsigned long align = clamp(vaddr & -vaddr, 16ul, PAGE_SIZE);
 	unsigned long offset = vaddr & (align - 1);
 	struct xrp_allocation *allocation;
 	long rc;