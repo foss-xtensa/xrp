foss-xtensa/xrp

commit 1c26b6771c4d5aa78b86861435ab62f4ae9eaed7

xrp-kernel: rearrange cache operations
- let the HW-specific code determine if a PFN range may be mapped cached
  or uncached by providing xrp_hw_ops::cacheable callback.
- combine xrp_hw_ops::clean_cache and xrp_hw_ops::flush_cache into
  xrp_hw_ops::dma_sync_for_device.
- provide xrp_hw_ops::dma_sync_for_cpu callback.
- add default implementations of all three that allow cached mappings in
  the system memory.

Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

xrp-kernel/xrp_hw_simple.c
@@ -158,36 +158,81 @@ static irqreturn_t irq_handler(int irq, void *dev_id)
 }
 
 #if defined(__XTENSA__)
-static void clean_cache(void *vaddr, phys_addr_t paddr, unsigned long sz)
+static bool cacheable(void *hw_arg, unsigned long pfn, unsigned long n_pages)
 {
-	__flush_dcache_range((unsigned long)vaddr, sz);
+	return true;
 }
 
-static void flush_cache(void *vaddr, phys_addr_t paddr, unsigned long sz)
+static void dma_sync_for_device(void *hw_arg,
+				void *vaddr, phys_addr_t paddr,
+				unsigned long sz, unsigned flags)
 {
-	__flush_dcache_range((unsigned long)vaddr, sz);
-	__invalidate_dcache_range((unsigned long)vaddr, sz);
+	switch (flags) {
+	case XRP_FLAG_READ:
+		__flush_dcache_range((unsigned long)vaddr, sz);
+		break;
+
+	case XRP_FLAG_READ_WRITE:
+		__flush_invalidate_dcache_range((unsigned long)vaddr, sz);
+		break;
+
+	case XRP_FLAG_WRITE:
+		__invalidate_dcache_range((unsigned long)vaddr, sz);
+		break;
+	}
 }
+
+static void dma_sync_for_cpu(void *hw_arg,
+			     void *vaddr, phys_addr_t paddr,
+			     unsigned long sz, unsigned flags)
+{
+	switch (flags) {
+	case XRP_FLAG_READ_WRITE:
+	case XRP_FLAG_WRITE:
+		__invalidate_dcache_range((unsigned long)vaddr, sz);
+		break;
+	}
+}
+
 #elif defined(__arm__)
-static void clean_cache(void *vaddr, phys_addr_t paddr, unsigned long sz)
+static bool cacheable(void *hw_arg, unsigned long pfn, unsigned long n_pages)
 {
-	__cpuc_flush_dcache_area(vaddr, sz);
-	outer_clean_range(paddr, paddr + sz);
+	return true;
 }
 
-static void flush_cache(void *vaddr, phys_addr_t paddr, unsigned long sz)
+static void dma_sync_for_device(void *hw_arg,
+				void *vaddr, phys_addr_t paddr,
+				unsigned long sz, unsigned flags)
 {
-	__cpuc_flush_dcache_area(vaddr, sz);
-	outer_flush_range(paddr, paddr + sz);
-}
-#else
-#warning "cache operations are not implemented for this architecture"
-static void clean_cache(void *vaddr, phys_addr_t paddr, unsigned long sz)
-{
+	switch (flags) {
+	case XRP_FLAG_READ:
+		__cpuc_flush_dcache_area(vaddr, sz);
+		outer_clean_range(paddr, paddr + sz);
+		break;
+
+	case XRP_FLAG_WRITE:
+		__cpuc_flush_dcache_area(vaddr, sz);
+		outer_inv_range(paddr, paddr + sz);
+		break;
+
+	case XRP_FLAG_READ_WRITE:
+		__cpuc_flush_dcache_area(vaddr, sz);
+		outer_flush_range(paddr, paddr + sz);
+		break;
+	}
 }
 
-static void flush_cache(void *vaddr, phys_addr_t paddr, unsigned long sz)
+static void dma_sync_for_cpu(void *hw_arg,
+			     void *vaddr, phys_addr_t paddr,
+			     unsigned long sz, unsigned flags)
 {
+	switch (flags) {
+	case XRP_FLAG_WRITE:
+	case XRP_FLAG_READ_WRITE:
+		__cpuc_flush_dcache_area(vaddr, sz);
+		outer_inv_range(paddr, paddr + sz);
+		break;
+	}
 }
 #endif
 
@@ -200,8 +245,11 @@ static const struct xrp_hw_ops hw_ops = {
 
 	.send_irq = send_irq,
 
-	.clean_cache = clean_cache,
-	.flush_cache = flush_cache,
+#if defined(__XTENSA__) || defined(__arm__)
+	.cacheable = cacheable,
+	.dma_sync_for_device = dma_sync_for_device,
+	.dma_sync_for_cpu = dma_sync_for_cpu,
+#endif
 };
 
 static long init_hw(struct platform_device *pdev, struct xrp_hw_simple *hw,

xrp-kernel/README.porting
@@ -89,16 +89,28 @@ notification. The hardware-specific XRP driver must install IRQ handler and
 call xrp_irq_handler function from it. The xrp_hw_ops::send_irq callback is
 used to send IRQ to the DSP.
 
-xrp_hw_ops::clean_cache and xrp_hw_ops::flush_cache are used to clean (write
-back) and flush (write back and invalidate) an area of user memory shared with
-the DSP. Standard linux cache management functions (from the DMA API) may be
-used only when the DSP shared memory is allocated from the system memory and
-covered by struct page's. If that's not the case the hardware-specific XRP
-driver may use architecture-specific methods of cache management. These
-functions are optional. If they are not provided then memory allocated from
-the driver is mapped into the userspace without caching and other userspace
-memory is shared directly with the DSP only when it's not cached, otherwise
-a kernel bounce buffer is used.
+xrp_hw_ops::cacheable determines if dma_sync_for_device and dma_sync_for_cpu
+are able to deal with a range of pages. It is called to determine whether the
+memory allocated from the XRP should be mapped cached or uncached to the
+userspace.
+It is also called for the ordinary userspace memory as well as for the 3rd
+party device memory userspace mappings to determine if the mapping may be
+shared with DSP directly or a bounce buffer is needed.
+
+xrp_hw_ops::dma_sync_for_device is used to clean (write back), flush (write
+back and invalidate) or invalidate an area of user memory shared with the DSP
+to transition ownership of that memory to the DSP. xrp_hw_ops::dma_sync_for_cpu
+is used to transition ownership of the memory back to host CPU.
+Standard linux cache management functions (from the DMA API) may be used only
+when the DSP shared memory is allocated from the system memory, covered by
+struct page's and there's no cache aliasing. If that's not the case then the
+hardware-specific XRP driver may use architecture-specific methods of cache
+management or forbid direct use of cached memory entirely.
+
+Cache-related functions are optional, if one of them is not provided then the
+default implementation is used: memory allocated from the driver is mapped into
+the userspace as cached only when it's a system memory and cached system memory
+is shared directly with the DSP.
 
 xrp_hw_ops::memcpy_toio and xrp_hw_ops::memset_io are optional functions used
 for copying data/code to the hardware-specific memory. memcpy_toio and

xrp-kernel/xrp_hw.h
@@ -52,8 +52,25 @@ struct xrp_hw_ops {
 	/* send IRQ to the core */
 	void (*send_irq)(void *hw_arg);
 
-	void (*clean_cache)(void *vaddr, phys_addr_t paddr, unsigned long sz);
-	void (*flush_cache)(void *vaddr, phys_addr_t paddr, unsigned long sz);
+	/*
+	 * check whether region of physical memory may be handled by
+	 * dma_sync_* operations
+	 */
+	bool (*cacheable)(void *hw_arg, unsigned long pfn, unsigned long n_pages);
+	/*
+	 * synchronize region of memory for DSP access.
+	 * flags: XRP_FLAG_{READ,WRITE,READWRITE}
+	 */
+	void (*dma_sync_for_device)(void *hw_arg,
+				    void *vaddr, phys_addr_t paddr,
+				    unsigned long sz, unsigned flags);
+	/*
+	 * synchronize region of memory for host access.
+	 * flags: XRP_FLAG_{READ,WRITE,READWRITE}
+	 */
+	void (*dma_sync_for_cpu)(void *hw_arg,
+				 void *vaddr, phys_addr_t paddr,
+				 unsigned long sz, unsigned flags);
 
 	/* memcpy data/code to device-specific memory */
 	void (*memcpy_tohw)(void __iomem *dst, const void *src, size_t sz);

xrp-kernel/xvp_main.c
@@ -85,7 +85,10 @@ struct xrp_mapping {
 		XRP_MAPPING_KERNEL = 0x4,
 	} type;
 	union {
-		struct xrp_allocation *xrp_allocation;
+		struct {
+			struct xrp_allocation *xrp_allocation;
+			unsigned long vaddr;
+		} native;
 		struct xrp_alien_mapping alien_mapping;
 	};
 };
@@ -126,27 +129,76 @@ static DEFINE_IDA(xvp_nodeid);
 
 static int xrp_boot_firmware(struct xvp *xvp);
 
-static const enum dma_data_direction xrp_dma_direction[] = {
-	[0] = DMA_NONE,
-	[XRP_FLAG_READ] = DMA_TO_DEVICE,
-	[XRP_FLAG_WRITE] = DMA_FROM_DEVICE,
-	[XRP_FLAG_READ_WRITE] = DMA_BIDIRECTIONAL,
-};
-
-static inline void xrp_dma_sync_for_device(struct xvp *xvp, phys_addr_t phys,
-					   unsigned long size,
-					   unsigned long flags)
+static bool xrp_cacheable(struct xvp *xvp, unsigned long pfn,
+			  unsigned long n_pages)
 {
-	dma_sync_single_for_device(xvp->dev, phys_to_dma(xvp->dev, phys), size,
-				   xrp_dma_direction[flags & XRP_FLAG_READ_WRITE]);
+	if (xvp->hw_ops->cacheable) {
+		return xvp->hw_ops->cacheable(xvp->hw_arg, pfn, n_pages);
+	} else {
+		unsigned long i;
+
+		for (i = 0; i < n_pages; ++i)
+			if (!pfn_valid(pfn + i))
+				return false;
+		return true;
+	}
 }
 
-static inline void xrp_dma_sync_for_cpu(struct xvp *xvp, phys_addr_t phys,
-					unsigned long size,
-					unsigned long flags)
+static int xrp_dma_direction(unsigned flags)
+{
+	static const enum dma_data_direction xrp_dma_direction[] = {
+		[0] = DMA_NONE,
+		[XRP_FLAG_READ] = DMA_TO_DEVICE,
+		[XRP_FLAG_WRITE] = DMA_FROM_DEVICE,
+		[XRP_FLAG_READ_WRITE] = DMA_BIDIRECTIONAL,
+	};
+	return xrp_dma_direction[flags & XRP_FLAG_READ_WRITE];
+}
+
+static void xrp_default_dma_sync_for_device(struct xvp *xvp,
+					    phys_addr_t phys,
+					    unsigned long size,
+					    unsigned long flags)
+{
+	dma_sync_single_for_device(xvp->dev, phys_to_dma(xvp->dev, phys), size,
+				   xrp_dma_direction(flags));
+}
+
+static void xrp_dma_sync_for_device(struct xvp *xvp,
+				    unsigned long virt,
+				    phys_addr_t phys,
+				    unsigned long size,
+				    unsigned long flags)
+{
+	if (xvp->hw_ops->dma_sync_for_device)
+		xvp->hw_ops->dma_sync_for_device(xvp->hw_arg,
+						 (void *)virt, phys, size,
+						 flags);
+	else
+		xrp_default_dma_sync_for_device(xvp, phys, size, flags);
+}
+
+static void xrp_default_dma_sync_for_cpu(struct xvp *xvp,
+					 phys_addr_t phys,
+					 unsigned long size,
+					 unsigned long flags)
 {
 	dma_sync_single_for_cpu(xvp->dev, phys_to_dma(xvp->dev, phys), size,
-				xrp_dma_direction[flags & XRP_FLAG_READ_WRITE]);
+				xrp_dma_direction(flags));
+}
+
+static void xrp_dma_sync_for_cpu(struct xvp *xvp,
+				 unsigned long virt,
+				 phys_addr_t phys,
+				 unsigned long size,
+				 unsigned long flags)
+{
+	if (xvp->hw_ops->dma_sync_for_cpu)
+		xvp->hw_ops->dma_sync_for_cpu(xvp->hw_arg,
+					      (void *)virt, phys, size,
+					      flags);
+	else
+		xrp_default_dma_sync_for_cpu(xvp, phys, size, flags);
 }
 
 static inline void xrp_comm_write32(volatile void __iomem *addr, u32 v)
@@ -434,21 +486,24 @@ static long xrp_ioctl_alloc(struct file *filp,
 	return 0;
 }
 
+static void xrp_put_pages(phys_addr_t phys, unsigned long n_pages)
+{
+	struct page *page;
+	unsigned long i;
+
+	page = pfn_to_page(__phys_to_pfn(phys));
+	for (i = 0; i < n_pages; ++i)
+		put_page(page + i);
+}
+
 static void xrp_alien_mapping_destroy(struct xrp_alien_mapping *alien_mapping)
 {
-	int i;
-	struct page *page;
-	int nr_pages;
-
 	switch (alien_mapping->type) {
 	case ALIEN_GUP:
-		page = pfn_to_page(__phys_to_pfn(alien_mapping->paddr));
-		nr_pages =
-			((alien_mapping->vaddr + alien_mapping->size +
-			  PAGE_SIZE - 1) >> PAGE_SHIFT) -
-			(alien_mapping->vaddr >> PAGE_SHIFT);
-		for (i = 0; i < nr_pages; ++i)
-			put_page(page + i);
+		xrp_put_pages(alien_mapping->paddr,
+			      PFN_UP(alien_mapping->vaddr +
+				     alien_mapping->size) -
+			      PFN_DOWN(alien_mapping->vaddr));
 		break;
 	case ALIEN_COPY:
 		xrp_allocation_put(alien_mapping->allocation);
@@ -600,7 +655,7 @@ static long _xrp_copy_user_phys(struct xvp *xvp,
 		size_t offs;
 
 		if (!to_phys)
-			xrp_dma_sync_for_cpu(xvp, paddr, size, flags);
+			xrp_default_dma_sync_for_cpu(xvp, paddr, size, flags);
 		for (offs = 0; offs < size; ++page) {
 			void *p = kmap(page);
 			size_t sz = PAGE_SIZE - page_offs;
@@ -629,7 +684,7 @@ static long _xrp_copy_user_phys(struct xvp *xvp,
 				return -EFAULT;
 		}
 		if (to_phys)
-			xrp_dma_sync_for_device(xvp, paddr, size, flags);
+			xrp_default_dma_sync_for_device(xvp, paddr, size, flags);
 	} else {
 		void __iomem *p = ioremap(paddr, size);
 		unsigned long rc;
@@ -762,7 +817,7 @@ static long xrp_share_kernel(struct file *filp,
 		mapping->type = XRP_MAPPING_KERNEL;
 		*paddr = phys;
 
-		xrp_dma_sync_for_device(xvp, phys, size, flags);
+		xrp_default_dma_sync_for_device(xvp, phys, size, flags);
 	}
 	pr_debug("%s: mapping = %p, mapping->type = %d\n",
 		 __func__, mapping, mapping->type);
@@ -863,36 +918,53 @@ static long __xrp_share_block(struct file *filp,
 
 		if (rc == 0) {
 			mapping->type = XRP_MAPPING_NATIVE;
-			mapping->xrp_allocation = xrp_allocation;
-			xrp_allocation_get(mapping->xrp_allocation);
+			mapping->native.xrp_allocation = xrp_allocation;
+			mapping->native.vaddr = virt;
+			xrp_allocation_get(xrp_allocation);
+			do_cache = vma_needs_cache_ops(vma);
 		}
 	}
 	if (rc < 0) {
 		struct xrp_alien_mapping *alien_mapping =
 			&mapping->alien_mapping;
+		unsigned long n_pages = PFN_UP(virt + size) - PFN_DOWN(virt);
 
 		/* Otherwise this is alien allocation. */
 		pr_debug("%s: non-XVP allocation at 0x%08lx\n",
 			 __func__, virt);
 
-		if (xvp->hw_ops->clean_cache ||
-		    (vma && !vma_needs_cache_ops(vma))) {
-			if (vma && vma->vm_flags & (VM_IO | VM_PFNMAP)) {
-				rc = xvp_pfn_virt_to_phys(xvp_file, vma,
-							  virt, size,
-							  &phys,
-							  alien_mapping);
-			} else {
-				up_read(&mm->mmap_sem);
-				rc = xvp_gup_virt_to_phys(xvp_file, virt,
-							  size, &phys,
-							  alien_mapping);
-				down_read(&mm->mmap_sem);
+		/*
+		 * A range can only be mapped directly if it is either
+		 * uncached or HW-specific cache operations can handle it.
+		 */
+		if (vma && vma->vm_flags & (VM_IO | VM_PFNMAP)) {
+			rc = xvp_pfn_virt_to_phys(xvp_file, vma,
+						  virt, size,
+						  &phys,
+						  alien_mapping);
+			if (rc == 0 && vma_needs_cache_ops(vma) &&
+			    !xrp_cacheable(xvp, PFN_DOWN(phys), n_pages)) {
+				pr_debug("%s: needs unsupported cache mgmt\n",
+					 __func__);
+				rc = -EINVAL;
 			}
 		} else {
-			pr_debug("%s: not mapping directly: no HW cache ops\n",
-				 __func__);
+			up_read(&mm->mmap_sem);
+			rc = xvp_gup_virt_to_phys(xvp_file, virt,
+						  size, &phys,
+						  alien_mapping);
+			if (rc == 0 &&
+			    (!vma || vma_needs_cache_ops(vma)) &&
+			    !xrp_cacheable(xvp, PFN_DOWN(phys), n_pages)) {
+				pr_debug("%s: needs unsupported cache mgmt\n",
+					 __func__);
+				xrp_put_pages(phys, n_pages);
+				rc = -EINVAL;
+			}
+			down_read(&mm->mmap_sem);
 		}
+		if (rc == 0 && vma && !vma_needs_cache_ops(vma))
+			do_cache = false;
 
 		/*
 		 * If we couldn't share try to make a shadow copy.
@@ -921,13 +993,10 @@ static long __xrp_share_block(struct file *filp,
 	pr_debug("%s: mapping = %p, mapping->type = %d\n",
 		 __func__, mapping, mapping->type);
 
-	if (do_cache && xvp->hw_ops->clean_cache) {
-		if (flags & XRP_FLAG_WRITE) {
-			xvp->hw_ops->flush_cache((void *)virt, phys, size);
-		} else if (flags & XRP_FLAG_READ) {
-			xvp->hw_ops->clean_cache((void *)virt, phys, size);
-		}
-	}
+	if (do_cache)
+		xrp_dma_sync_for_device(xvp,
+					virt, phys, size,
+					flags);
 	return 0;
 }
 
@@ -942,6 +1011,11 @@ static long xrp_writeback_alien_mapping(struct xvp_file *xvp_file,
 
 	switch (alien_mapping->type) {
 	case ALIEN_GUP:
+		xrp_dma_sync_for_cpu(xvp_file->xvp,
+				     alien_mapping->vaddr,
+				     alien_mapping->paddr,
+				     alien_mapping->size,
+				     flags);
 		pr_debug("%s: dirtying alien GUP @va = %p, pa = %pap\n",
 			 __func__, (void __user *)alien_mapping->vaddr,
 			 &alien_mapping->paddr);
@@ -986,7 +1060,17 @@ static long __xrp_unshare_block(struct file *filp, struct xrp_mapping *mapping,
 
 	switch (mapping->type & ~XRP_MAPPING_KERNEL) {
 	case XRP_MAPPING_NATIVE:
-		xrp_allocation_put(mapping->xrp_allocation);
+		if (flags & XRP_FLAG_WRITE) {
+			struct xvp_file *xvp_file = filp->private_data;
+
+			xrp_dma_sync_for_cpu(xvp_file->xvp,
+					     mapping->native.vaddr,
+					     mapping->native.xrp_allocation->start,
+					     mapping->native.xrp_allocation->size,
+					     flags);
+
+		}
+		xrp_allocation_put(mapping->native.xrp_allocation);
 		break;
 
 	case XRP_MAPPING_ALIEN:
@@ -1506,8 +1590,11 @@ static int xvp_mmap(struct file *filp, struct vm_area_struct *vma)
 		struct xvp *xvp = xvp_file->xvp;
 		pgprot_t prot = vma->vm_page_prot;
 
-		if (!xvp->hw_ops->clean_cache)
+		if (!xrp_cacheable(xvp, pfn,
+				   PFN_DOWN(vma->vm_end - vma->vm_start))) {
 			prot = pgprot_writecombine(prot);
+			vma->vm_page_prot = prot;
+		}
 
 		err = remap_pfn_range(vma, vma->vm_start, pfn,
 				      vma->vm_end - vma->vm_start,
@@ -1753,9 +1840,10 @@ static long xrp_init_common(struct platform_device *pdev,
 {
 	long ret;
 	char nodename[sizeof("xvp") + 3 * sizeof(int)];
-	struct xvp *xvp = devm_kzalloc(&pdev->dev, sizeof(*xvp), GFP_KERNEL);
+	struct xvp *xvp;
 	int nodeid;
 
+	xvp = devm_kzalloc(&pdev->dev, sizeof(*xvp), GFP_KERNEL);
 	if (!xvp) {
 		ret = -ENOMEM;
 		goto err;
@@ -1891,18 +1979,8 @@ static void *get_hw_sync_data(void *hw_arg, size_t *sz)
 	return p;
 }
 
-static void clean_cache(void *vaddr, phys_addr_t paddr, unsigned long sz)
-{
-}
-
-static void flush_cache(void *vaddr, phys_addr_t paddr, unsigned long sz)
-{
-}
-
 static const struct xrp_hw_ops hw_ops = {
 	.get_hw_sync_data = get_hw_sync_data,
-	.clean_cache = clean_cache,
-	.flush_cache = flush_cache,
 };
 
 #ifdef CONFIG_OF

commit d328e854aa5d9d249fe1a16ef00393cdee251a15

xrp-kernel: fix build as module
Include missing xrp_kernel_defs.h for XRP_FLAG_* definitions.
Use __flush_dcache_range + __invalidate_dcache_range instead of
non-exported __flush_invalidate_dcache_range.

Fixes: 1c26b6771c4d ("xrp-kernel: rearrange cache operations")
Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

xrp-kernel/xrp_hw_simple.c
@@ -35,6 +35,7 @@
 #include <linux/platform_device.h>
 #include <linux/slab.h>
 #include <asm/cacheflush.h>
+#include "xrp_kernel_defs.h"
 #include "xrp_hw.h"
 #include "xrp_hw_simple_dsp_interface.h"
 
@@ -173,7 +174,8 @@ static void dma_sync_for_device(void *hw_arg,
 		break;
 
 	case XRP_FLAG_READ_WRITE:
-		__flush_invalidate_dcache_range((unsigned long)vaddr, sz);
+		__flush_dcache_range((unsigned long)vaddr, sz);
+		__invalidate_dcache_range((unsigned long)vaddr, sz);
 		break;
 
 	case XRP_FLAG_WRITE: