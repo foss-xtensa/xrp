foss-xtensa/xrp

commit 7162d9b1af5bd30e662575b34f275eee51658356

xrp-common: implement ring buffer file
Define ring buffer structure and provide a function xrp_make_rb_file
that returns FILE pointer associated with the ring buffer.

Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

xrp-common/xrp_rb_file.c
@@ -1,0 +1,94 @@
+/*
+ * Copyright (c) 2018 Cadence Design Systems Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <string.h>
+#include "xrp_rb_file.h"
+
+#ifdef HAVE_OPENCOOKIE
+
+static ssize_t rb_write(void *cookie, const char *buf, size_t size)
+{
+	volatile struct xrp_ring_buffer *rb = cookie;
+	uint32_t read = rb->read;
+	uint32_t write = rb->write;
+	size_t total;
+	size_t tail;
+
+	tail = rb->size - write;
+	if (read > write) {
+		total = read - 1 - write;
+		tail = total;
+	} else if (read == write) {
+		total = rb->size - 1;
+	} else {
+		total = rb->size - 1 - write + read;
+		if (total < tail)
+			tail = total;
+	}
+
+	if (size < tail)
+		tail = size;
+
+	memcpy((char *)rb->data + write, buf, tail);
+	buf += tail;
+	write += tail;
+	if (write == rb->size)
+		write = 0;
+	size -= tail;
+	total -= tail;
+	if (size && total) {
+		if (size < total)
+			total = size;
+		memcpy((char *)rb->data, buf, total);
+		write += total;
+	} else {
+		total = 0;
+	}
+	rb->write = write;
+	return tail + total;
+}
+
+FILE *xrp_make_rb_file(struct xrp_ring_buffer *rb)
+{
+	static cookie_io_functions_t rb_ops = {
+		.write = rb_write,
+	};
+	FILE *f = fopencookie(rb, "w", rb_ops);
+
+	if (f)
+		setvbuf(f, NULL, _IONBF, 0);
+
+	return f;
+}
+
+#else
+
+FILE *xrp_make_rb_file(struct xrp_ring_buffer *rb)
+{
+	(void *)rb;
+	return NULL;
+}
+
+#endif

configure.ac
@@ -176,6 +176,8 @@ AS_IF([test x$build_dsp = xfalse -a x$build_example = xtrue -a x$build_standalon
 	    [AC_MSG_FAILURE([No usable dtc is found])])
       ])
 
+AC_CHECK_FUNC([fopencookie],[AC_DEFINE([HAVE_OPENCOOKIE])])
+
 HW_PORT=simple
 AC_ARG_ENABLE(port,
 	      [AS_HELP_STRING([--enable-port[[=ARG]]],

xrp-common/xrp_rb_file.h
@@ -1,0 +1,33 @@
+/*
+ * Copyright (c) 2018 Cadence Design Systems Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef XRP_RB_FILE_H
+#define XRP_RB_FILE_H
+
+#include <stdio.h>
+#include <xrp_types.h>
+#include <xrp_ring_buffer.h>
+
+FILE *xrp_make_rb_file(struct xrp_ring_buffer *rb);
+
+#endif

xrp-dsp/Makefile.am
@@ -29,4 +29,7 @@ AM_CFLAGS = -W -Wall
 include_HEADERS = ../xrp_api.h xrp_dsp_hw.h
 lib_LIBRARIES = libxrp-dsp.a
 
-libxrp_dsp_a_SOURCES = xrp_dsp.c ../xrp-common/xrp_ns.c xrp_dsp_framework.c
+libxrp_dsp_a_SOURCES = xrp_dsp.c \
+		       xrp_dsp_framework.c \
+		       ../xrp-common/xrp_ns.c \
+		       ../xrp-common/xrp_rb_file.c

xrp-kernel/xrp_ring_buffer.h
@@ -1,0 +1,40 @@
+/*
+ * XRP ring buffer structure
+ *
+ * Copyright (c) 2018 Cadence Design Systems, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Alternatively you can use and distribute this file under the terms of
+ * the GNU General Public License version 2 or later.
+ */
+
+#ifndef XRP_RING_BUFFER_H
+#define XRP_RING_BUFFER_H
+
+struct xrp_ring_buffer {
+	__u32 read;
+	__u32 write;
+	__u32 size;
+	__u32 reserved[1];
+	__u8 data[0];
+};
+
+#endif

commit 7b69ea8ac75a0edd7b1d727c6f8b642c078a0d22

xrp-common: bring ssize_t definition in via unistd.h
This fixes build error when building with uclibc-ng.

Fixes: 7162d9b1af5b ("xrp-common: implement ring buffer file")
Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

xrp-common/xrp_rb_file.c
@@ -24,6 +24,7 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include "xrp_rb_file.h"
 
 #ifdef HAVE_OPENCOOKIE