foss-xtensa/xrp

commit 16f76bf16e1edfe2c586c3f9f30c82d247ec2216

xrp-dsp: support DSP cores without sync option
Use memw instruction together with ordinary loads/stores on DSP cores
that don't have MP sync option enabled.

Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

xrp-dsp/xrp_dsp.c
@@ -25,12 +25,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <xtensa/tie/xt_sync.h>
 #include <xtensa/xtruntime.h>
 
 #include "xrp_api.h"
 #include "xrp_debug.h"
 #include "xrp_dsp_hw.h"
+#include "xrp_dsp_sync.h"
 #include "xrp_ns.h"
 #include "xrp_types.h"
 #include "xrp_kernel_dsp_interface.h"
@@ -294,19 +294,19 @@ static void do_handshake(struct xrp_dsp_sync *shared_sync)
 
 	pr_debug("%s, shared_sync = %p\n", __func__, shared_sync);
 
-	while (XT_L32AI(&shared_sync->sync, 0) != XRP_DSP_SYNC_START) {
+	while (xrp_l32ai(&shared_sync->sync) != XRP_DSP_SYNC_START) {
 		dcache_region_invalidate(&shared_sync->sync,
 					 sizeof(shared_sync->sync));
 	}
 
-	XT_S32RI(XRP_DSP_SYNC_DSP_READY, &shared_sync->sync, 0);
+	xrp_s32ri(XRP_DSP_SYNC_DSP_READY, &shared_sync->sync);
 	dcache_region_writeback(&shared_sync->sync,
 				sizeof(shared_sync->sync));
 
 	for (;;) {
 		dcache_region_invalidate(&shared_sync->sync,
 					 sizeof(shared_sync->sync));
-		v = XT_L32AI(&shared_sync->sync, 0);
+		v = xrp_l32ai(&shared_sync->sync);
 		if (v == XRP_DSP_SYNC_HOST_TO_DSP)
 			break;
 		if (v != XRP_DSP_SYNC_DSP_READY)
@@ -315,7 +315,7 @@ static void do_handshake(struct xrp_dsp_sync *shared_sync)
 
 	xrp_hw_set_sync_data(shared_sync->hw_sync_data);
 
-	XT_S32RI(XRP_DSP_SYNC_DSP_TO_HOST, &shared_sync->sync, 0);
+	xrp_s32ai(XRP_DSP_SYNC_DSP_TO_HOST, &shared_sync->sync);
 	dcache_region_writeback(&shared_sync->sync,
 				sizeof(shared_sync->sync));
 
@@ -329,7 +329,7 @@ static void do_handshake(struct xrp_dsp_sync *shared_sync)
 static inline int xrp_request_valid(struct xrp_dsp_cmd *dsp_cmd,
 				    uint32_t *pflags)
 {
-	uint32_t flags = XT_L32AI(&dsp_cmd->flags, 0);
+	uint32_t flags = xrp_l32ai(&dsp_cmd->flags);
 
 	*pflags = flags;
 	return (flags & (XRP_DSP_CMD_FLAG_REQUEST_VALID |
@@ -344,7 +344,7 @@ static void complete_request(struct xrp_dsp_cmd *dsp_cmd, uint32_t flags)
 
 	dcache_region_writeback(dsp_cmd,
 				sizeof(*dsp_cmd));
-	XT_S32RI(flags, &dsp_cmd->flags, 0);
+	xrp_s32ri(flags, &dsp_cmd->flags);
 	dcache_region_writeback(&dsp_cmd->flags,
 				sizeof(dsp_cmd->flags));
 	xrp_hw_send_host_irq();

configure.ac
@@ -156,7 +156,12 @@ AC_ARG_WITH([libfdt],
 AC_SUBST(LIBFDT_CPPFLAGS)
 AC_SUBST(LIBFDT_LDFLAGS)
 
-AS_IF([test x$build_dsp = xfalse],
+AS_IF([test x$build_dsp = xtrue],
+      [
+      AC_CHECK_HEADERS([xtensa/tie/xt_sync.h])
+      ])
+
+AS_IF([test x$build_host = xtrue],
       [
       AC_MSG_CHECKING([for working _Atomic support])
       AC_COMPILE_IFELSE([

xrp-dsp/simple/xrp_dsp_hw_simple.c
@@ -24,11 +24,11 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <xtensa/tie/xt_interrupt.h>
-#include <xtensa/tie/xt_sync.h>
 #include <xtensa/xtruntime.h>
 
-#include "xrp_dsp_hw.h"
 #include "xrp_debug.h"
+#include "xrp_dsp_hw.h"
+#include "xrp_dsp_sync.h"
 #include "xrp_types.h"
 #include "xrp_hw_simple_dsp_interface.h"
 
@@ -57,17 +57,17 @@ static void xrp_irq_handler(void)
 {
 	pr_debug("%s\n", __func__);
 	if (device_irq_mode == XRP_IRQ_LEVEL)
-		XT_S32RI(0, device_mmio(device_irq_offset), 0);
+		xrp_s32ri(0, device_mmio(device_irq_offset));
 }
 
 void xrp_hw_send_host_irq(void)
 {
 	switch (host_irq_mode) {
 	case XRP_IRQ_EDGE:
-		XT_S32RI(0, host_mmio(host_irq_offset), 0);
+		xrp_s32ri(0, host_mmio(host_irq_offset));
 		/* fall through */
 	case XRP_IRQ_LEVEL:
-		XT_S32RI(1u << host_irq_bit, host_mmio(host_irq_offset), 0);
+		xrp_s32ri(1u << host_irq_bit, host_mmio(host_irq_offset));
 		break;
 	default:
 		break;

xrp-dsp/xrp_dsp_sync.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 - 2017 Cadence Design Systems Inc.
+ * Copyright (c) 2018 Cadence Design Systems Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
@@ -21,43 +21,38 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#include <stdint.h>
-#include <stdio.h>
-#include <xtensa/xtruntime.h>
+#ifndef XRP_DSP_SYNC_H
+#define XRP_DSP_SYNC_H
 
-#include "xrp_api.h"
-#include "xrp_dsp_hw.h"
+#ifdef HAVE_XTENSA_TIE_XT_SYNC_H
+#include <xtensa/tie/xt_sync.h>
+#else
+#include <xtensa/tie/xt_core.h>
+#endif
 
-static enum xrp_status run_command_loop(void)
+#ifdef HAVE_XTENSA_TIE_XT_SYNC_H
+static inline uint32_t xrp_l32ai(volatile void *p)
 {
-	enum xrp_status status;
-	struct xrp_device *device = xrp_open_device(0, &status);
-
-	if (status != XRP_STATUS_SUCCESS)
-		return status;
-
-	for (;;) {
-		status = xrp_device_dispatch(device);
-		if (status == XRP_STATUS_PENDING)
-			xrp_hw_wait_device_irq();
-		else if (status != XRP_STATUS_SUCCESS)
-			return status;
-	}
+	return XT_L32AI(p, 0);
 }
 
-void xrp_user_initialize(enum xrp_status *status) __attribute__((weak));
-void xrp_user_initialize(enum xrp_status *status)
+static inline void xrp_s32ri(uint32_t v, volatile void *p)
 {
-	*status = XRP_STATUS_SUCCESS;
+	XT_S32RI(v, p, 0);
+}
+#else
+static inline uint32_t xrp_l32ai(volatile void *p)
+{
+	uint32_t v = *(const volatile uint32_t *)p;
+	XT_MEMW();
+	return v;
 }
 
-int main()
+static inline void xrp_s32ri(uint32_t v, volatile void *p)
 {
-	enum xrp_status status = XRP_STATUS_SUCCESS;
-
-	xrp_user_initialize(&status);
-
-	if (status != XRP_STATUS_SUCCESS)
-		return status;
-	return run_command_loop();
+	XT_MEMW();
+	*(volatile uint32_t *)p = v;
 }
+#endif
+
+#endif

commit 4a14e4c2e288e55362ad7df3658b3e9ffbe1f38b

xrp-dsp/xrp_dsp.c: fix do_handshake
do_handshake incorrectly used xrp_s32ai instead of xrp_s32ri.

Fixes: 16f76bf16e1e ("xrp-dsp: support DSP cores without sync option")
Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

xrp-dsp/xrp_dsp.c
@@ -315,7 +315,7 @@ static void do_handshake(struct xrp_dsp_sync *shared_sync)
 
 	xrp_hw_set_sync_data(shared_sync->hw_sync_data);
 
-	xrp_s32ai(XRP_DSP_SYNC_DSP_TO_HOST, &shared_sync->sync);
+	xrp_s32ri(XRP_DSP_SYNC_DSP_TO_HOST, &shared_sync->sync);
 	dcache_region_writeback(&shared_sync->sync,
 				sizeof(shared_sync->sync));
 