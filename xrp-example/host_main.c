foss-xtensa/xrp

commit 8865785160b1dafe3f5eb78d7da357a6da2201ca

xrp-example: fix build for xtensa hosts without HW atomics
xclib for xtensa hosts without hardware atomics support requires that
function _xt_atomic_compare_exchange_4 is defined by the user.
Provide this definitions in the host-side example code when building
for xtensa.

Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

xrp-example/host_main.c
@@ -31,6 +31,9 @@
 #ifdef HAVE_THREADS_XOS
 #include <xtensa/xos.h>
 #endif
+#ifdef __xtensa__
+#include <xtensa/hal.h>
+#endif
 
 /* Test data transfer from and to in/out buffers */
 static void f1(int devid)
@@ -426,6 +429,15 @@ enum {
 
 	CMD_N,
 };
+
+#ifdef __xtensa__
+int _xt_atomic_compare_exchange_4(unsigned int *_ptr,
+				  unsigned int _exp,
+				  unsigned int _val)
+{
+	return xthal_compare_and_set((int32_t *)_ptr, _exp, _val);
+}
+#endif
 
 int main(int argc, char **argv)
 {

commit c7363c251c552eaede46897d4bb590db421e1c17

xrp-example: fix inclusion of xtensa/hal.
Don't blindly include it on any __xtensa__ target, only do it when the
header is available. This fixes example build on xtensa linux host.

Fixes: 8865785160b1 ("xrp-example: fix build for xtensa hosts without HW
atomics")
Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

configure.ac
@@ -178,6 +178,7 @@ AS_IF([test x$build_host = xtrue],
 AS_IF([test x$build_host = xtrue -a x$build_standalone = xtrue],
       [
       AC_CHECK_HEADERS([valgrind/memcheck.h])
+      AC_CHECK_HEADERS([xtensa/hal.h])
       ])
 
 AS_IF([test x$build_host = xtrue -a x$build_standalone = xtrue -a x$build_example = xtrue],

xrp-example/host_main.c
@@ -31,7 +31,7 @@
 #ifdef HAVE_THREADS_XOS
 #include <xtensa/xos.h>
 #endif
-#ifdef __xtensa__
+#ifdef HAVE_XTENSA_HAL_H
 #include <xtensa/hal.h>
 #endif
 
@@ -430,7 +430,7 @@ enum {
 	CMD_N,
 };
 
-#ifdef __xtensa__
+#ifdef HAVE_XTENSA_HAL_H
 int _xt_atomic_compare_exchange_4(unsigned int *_ptr,
 				  unsigned int _exp,
 				  unsigned int _val)